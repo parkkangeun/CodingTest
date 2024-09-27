00f45522b5ad23ad95e515887e31f322d58af120
parkkangeun <kangeun24@icloud.com>
2024-09-27 16:52:14 +0900

    0927

--- Programmers/Lv.01/콜라문제.py
+++ Programmers/Lv.01/콜라문제.py
@@ -1,12 +1,6 @@
 def solution(a, b, n):
     answer = 0
-    temp = 0
-    while a <= n:
-        temp = n % a 
-        n = (n // a) * b
-        answer += n
-        n += temp
+    while n >= a:
+        answer += (n // a) * b
+        n = (n // a) * b + (n % a)
     return answer
-
-
-print(solution(2, 1, 20)) 
