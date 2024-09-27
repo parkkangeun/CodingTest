ae89661c6b330ec4d76a0d539aea8953f8270bcb
parkkangeun <kangeun24@icloud.com>
2024-09-27 15:40:45 +0900

    remove

--- Programmers/삼총사.py
+++
@@ -1,9 +0,0 @@
-def solution(number):
-    length = len(number)
-    answer = 0
-    for i in range(length):
-        for ii in range(i + 1, length):
-            for iii in range(ii + 1, length):
-                if number[i] + number[ii] + number[iii] == 0:
-                    answer += 1
-    return answer
