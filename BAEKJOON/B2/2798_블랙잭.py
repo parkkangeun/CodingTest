9b970d807805313c39e61da539ffd615dfb63737
parkkangeun <kangeun24@icloud.com>
2024-10-01 14:17:03 +0900

    2024.10.01

--- BAEKJOON/B2/2798_블랙잭.py
+++ BAEKJOON/B2/2798_블랙잭.py
@@ -1,2 +1,15 @@
 # 2024.09.30
 # Python
+
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+answer = 0
+for i in range(n):
+    for ii in range(i + 1, n):
+        for iii in range(ii + 1, n):
+            num_sum = numbers[i] + numbers[ii] + numbers[iii]
+            if num_sum == m or answer < num_sum < m:
+                answer = num_sum
+    if answer == m:
+        break
+print(answer)
