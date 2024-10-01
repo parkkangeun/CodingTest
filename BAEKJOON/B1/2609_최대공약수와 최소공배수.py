8442032fb998a14ec13f388322d1bb1b7f443ed1
parkkangeun <kangeun24@icloud.com>
2024-10-01 15:57:41 +0900

    2024.10.01 유클리드 호제법 최대공약수와 최소공배수

--- BAEKJOON/B1/2609_최대공약수와 최소공배수.py
+++ BAEKJOON/B1/2609_최대공약수와 최소공배수.py
@@ -1,12 +1,15 @@
 # 2024.10.01
 # Python
 
+# 최대공약수
 def gcd(a, b):
     while b != 0:
         r = a % b
         a = b
         b = r
     return a
+
+# 최소공배수
 def lcm(a, b):
     return (a * b) / gcd(a, b)
 
