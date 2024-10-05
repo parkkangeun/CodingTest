f5f4261b13dd58a9769537261366c4f0aa794157
parkkangeun <kangeun24@icloud.com>
2024-10-05 14:27:57 +0900

    2024.10.05

---
+++ BAEKJOON/S5/2751_수 정렬하기 2.py
@@ -0,0 +1,13 @@
+# 2024.10.05
+# Python
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = []
+for _ in range(n):
+    numbers.append(int(input()))
+numbers.sort()
+for i in numbers:
+    print(i)
