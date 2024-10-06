9303f20f9376e0c98cbbe3e633edd5db042d3772
parkkangeun <kangeun24@icloud.com>
2024-10-06 23:12:04 +0900

    2024.10.06

---
+++ BAEKJOON/S4/1018_체스판 다시 칠하기.py
@@ -0,0 +1,30 @@
+# 2024.10.06
+# Python
+
+n, m = map(int, input().split())
+result = []
+board = []
+for i in range(n):
+    board.append(input())
+
+for i in range(n - 7):
+    for j in range(m- 7):
+        draw_01 = 0
+        draw_02 = 0
+        
+        for ii in range(i, i + 8):
+            for jj in range(j, j + 8):
+                if (ii + jj) % 2 == 0:
+                    if board[ii][jj] != 'B':
+                        draw_01 += 1
+                    if board[ii][jj] != 'W':
+                        draw_02 += 1
+                else:
+                    if board[ii][jj] != 'W':
+                        draw_01 += 1
+                    if board[ii][jj] != 'B':
+                        draw_02 += 1
+        result.append(draw_01)
+        result.append(draw_02)
+print(result)
+print(min(result))
