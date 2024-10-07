19d5db9b2ec09104beb404be2870cdb8ba8694b8
parkkangeun <kangeun24@icloud.com>
2024-10-07 12:23:48 +0900

    2024.10.07 deque 공부하기

---
+++ BAEKJOON/S4/2164_카드.py
@@ -0,0 +1,31 @@
+# 2024.10.07
+# Python
+
+# import queue
+# n = int(input())
+# que = queue.Queue()
+# for i in range(1, n + 1):
+#     que.put(i)
+# while True:
+#     que.get()
+#     if que.qsize() == 1:
+#         break
+#     que.put(que.get())
+# print(que.get())
+
+# cards = [i for i in range(1, int(input()) + 1)]
+
+# while True:
+#     cards.pop(0)
+#     if len(cards) == 1:
+#         break
+#     cards.append(cards[0])
+#     cards.pop(0)
+# print(*cards)
+
+from collections import deque
+
+n = int(input())
+
+deque = deque([i for i in range(1, n + 1)])
+print(deque)
