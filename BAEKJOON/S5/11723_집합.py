7b7db21776a2645c5fb01307f1509be3a9aba528
parkkangeun <kangeun24@icloud.com>
2024-10-13 23:07:45 +0900

    2024.10.13

--- BAEKJOON/S5/11723_집합.py
+++ BAEKJOON/S5/11723_집합.py
@@ -8,24 +8,24 @@ m = int(stdin.readline().rstrip())
 s = []
 
 for _ in range(m):
-    cal = stdin.readline().rstrip()
-    if cal == 'all':
+    opr = stdin.readline().rstrip()
+    if opr == 'all':
         s = [i for i in range(1, 21)]
-    elif cal == 'empty':
+    elif opr == 'empty':
             s = []
     else:
-        cal, num = cal.split()
+        opr, num = opr.split()
         num =  int(num)
-        if cal == 'add' and num not in s:
+        if opr == 'add' and num not in s:
             s.append(num)
-        elif cal == 'remove' and num in s:
+        elif opr == 'remove' and num in s:
             s.remove(num)
-        elif cal == 'check':
+        elif opr == 'check':
             if num in s:
                 print(1)
             else:
                 print(0)
-        elif cal == 'toggle':
+        elif opr == 'toggle':
             if num in s:
                 s.remove(num)
             else:
