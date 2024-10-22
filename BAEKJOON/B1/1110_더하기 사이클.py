e328f3117b244b5a53a90bdd1baa6b06b065843f
parkkangeun <kangeun24@icloud.com>
2024-10-22 13:53:54 +0900

    2024.10.22

---
+++ BAEKJOON/B1/1110_더하기 사이클.py
@@ -0,0 +1,20 @@
+# 2024.10.22
+# Python
+
+# n 입력받기
+n = int(input())
+
+# n의 값 복사
+n_copy = n
+
+cnt = 0
+# 계산
+while True:
+    cnt += 1
+    n = (n % 10) * 10 + ((n // 10) + (n % 10)) % 10
+    print(n, n_copy)
+    if n == n_copy:
+        break
+
+# 출력
+print(cnt)
