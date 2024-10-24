cf16a43c2758a6df51e282135d9b2dcb9fdcb44e
parkkangeun <kangeun24@icloud.com>
2024-10-24 13:31:40 +0900

    2024.10.24

---
+++ BAEKJOON/S5/1268_임시 반장 정하기.py
@@ -0,0 +1,27 @@
+# 2024.10.24
+# Python
+
+from sys import stdin
+input = stdin.readline
+# 학생 수 받기
+num = int(input())
+same = [0] * num
+
+# num명의 학생들이 1 ~ 5학년까지 속한 반 받기
+std = []
+for i in range(num):
+    std.append(list(map(int, input().split())))
+
+# 같은 반에 속했던 학생의 유무 
+for i in range(num):
+    for j in range(i + 1,  num):
+        for grade in range(5):
+            if std[i][grade] == std[j][grade]:
+                same[i] += 1
+                same[j] += 1
+                break
+            
+# 최대값을 가진 학생의 인덱스 출력
+print(same.index(max(same)) + 1)
+
+
